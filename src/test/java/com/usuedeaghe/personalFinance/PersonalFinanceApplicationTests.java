a7ce9b81efded77fe81f4e04ec07d07b2e2ba484
Usu <usuedeaghe@gmail.com>
2024-08-02 17:06:38 +0100

    initial commit

---
+++ src/test/java/com/usuedeaghe/personalFinance/PersonalFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuedeaghe.personalFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
